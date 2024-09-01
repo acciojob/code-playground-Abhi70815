5c5bce4e14b5585aa2e1d906e6d784cab78155b7
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-01 15:26:13 +0530

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import { Link, Route, Routes } from 'react-router-dom';
-import Login from './Login';
-import PlayGround from './PlayGround'
-import Error from './Error';
+
+import React, { useEffect, useState } from "react";
+import './../styles/App.css';
+import Login from "./Login";
+import PlayGround from "./PlayGround";
+import { Link, Route, Routes } from "react-router-dom";
+import Error from "./Error";
 
 
 const App = () => {
-    const [message, setMessage]=useState('you are not authenticated, Please login first');
-    const [islogin, setLogin]= useState(false);
-    const [path, setPath]= useState('/login');
-    const [btn, setbtn]=useState('Log In');
-    useEffect(()=>{
-        if(islogin){
-            setPath('/playground');
-        }
-        else{
-            setPath('/login');
-        }
-    },[islogin]);
-    console.log(islogin)
-
+  const [message,setmessage]=useState('You are not authenticated, Please login first');
+  const [islogin,setislogin]=useState(false);
+  const [path,setpath]=useState('/Login');
+  const [btn,setbtn]=useState('Log In');
+  useEffect(()=>{
+    if (islogin) {
+      setpath('/playground');
+    }else{
+      setpath('/login');
+    }
+ 
+  },[islogin]);
   return (
-    <div className='main-container'>
- <p>{message}</p>
- <ul>
-    <li><Link to={path}>PlayGround</Link></li>
-    <li><Link to="/login">Login</Link></li>
- </ul>
- <Routes>
-    <Route path='/login' element={<Login btn={btn} setbtn={setbtn} islogin={islogin} setLogin={setLogin} message={setMessage}/>}>Login</Route>
-    <Route path='/playground' element={<PlayGround/>} >PlayGround</Route>
-    <Route path='*' element={<Error/>}>NotFound</Route>
- </Routes>
+    <div className="main-container">
+     <p>{message}</p>
+     <ul>
+      <li><a><Link to={path}>PlayGround</Link></a></li>
+      <li><a><Link to="/login">Login</Link></a></li>
+     </ul>
+      {/* <a href={path}>PlayGround</a><br/>
+      <a href="/login">Login</a> */}
+      <Routes>
+        <Route path="/login" element={ <Login btn={btn} setbtn={setbtn} islogin={islogin} setislogin={setislogin} message={setmessage} />} >Login</Route>
+        <Route path="/playground" element={<PlayGround />}>PlayGround</Route>
+        <Route path="*" element={<Error/>}>NotFound</Route>
+        {/* <Route path="/NotFound" element={<NotFound />}>PlayGround</Route> */}
+      </Routes>
+        {/* Do not remove the main div */}
     </div>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,24 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 const Login = (props) => {
- const handlelogin=()=>[
-  props.setbtn(btn=>{
-    if(btn=='Log In'){
-      props.setLogin(true);
-      props.message('Logged in, Now you can enter playground');
-      return 'Log Out';
-    }
-    else{
-      props.setLogin(false);
-      props.message('You are not authenticated, Please login first');
-      return 'Log In'
-    }
-  })
- ]
+  
+const handlelogin=()=>[
+    props.setbtn(btn=>{
+        if (btn=='Log In') {
+            props.setislogin(true);
+            props.message('Logged in, Now you can enter Playground');
+            return 'Log Out';
+        }else{
+            props.setislogin(false);
+            props.message('You are not authenticated, Please login first');
+            return 'Log In';
+        }
+    })
+]
   return (
     <div>
-      <p>Login</p>
-      <button onClick={handlelogin}>{props.btn}</button>
+        <p>Login</p>
+        <button onClick={handlelogin}>{props.btn}</button>
     </div>
   )
 }
